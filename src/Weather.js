c458848fb89993f0f98f980cc40523a58d7ed04b
yasmeencardenas <123997412+yasmeencardenas@users.noreply.github.com>
2024-09-02 20:41:30 +0300

    Created React App

---
+++ src/Weather.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./styles.css";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coordinates,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      icon: response.data.condition.icon_url,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Search for a city"
+        onChange={handleCityChange}
+      />
+      <input type="submit" value="search" />
+    </form>
+  );
+
+  if (weatherData.ready) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weatherData.temperature)}°C</li>
+          <li>Description: {weatherData.description}</li>
+          <li>Humidity: {weatherData.humidity}%</li>
+          <li>Wind: {weatherData.wind}km/h</li>
+          <li>
+            {" "}
+            <img src={weatherData.icon} alt={weatherData.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
