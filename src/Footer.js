b48309b96fd8c334141050eda83b6b4162d6e7b0
yasmeencardenas <123997412+yasmeencardenas@users.noreply.github.com>
2024-09-04 23:08:40 +0300

    Created Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      This project was coded by{" "}
+      <a
+        href="https://github.com/yasmeencardenas"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Yasmeen Cardenas{" "}
+      </a>
+      and is{" "}
+      <a
+        href="https://github.com/yasmeencardenas/shecodes-react-week4-weather-project"
+        target="_blank"
+        rel="noreferrer"
+      >
+        on GitHub{" "}
+      </a>
+      and{" "}
+      <a
+        href="https://app.netlify.com/sites/sprightly-starship-b5a630/overview"
+        target="_blank"
+        rel="noreferrer"
+      >
+        hosted on Netlify
+      </a>
+    </p>
+  );
+}
